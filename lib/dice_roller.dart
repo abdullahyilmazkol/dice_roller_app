ff998f4cc383dc33c7b4ec3fe61a2f0a7881aa71
abdullahyilmazkol <58661477+abdullahyilmazkol@users.noreply.github.com>
2023-11-18 21:04:59 +0300

    Add files via upload

---
+++ lib/dice_roller.dart
@@ -0,0 +1,38 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+
+  void onPressed() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset("images/dice-$currentDiceRoll.png"),
+        TextButton(
+          onPressed: onPressed,
+          child: const StyledText("Roll Dice"),
+        ),
+      ],
+    );
+  }
+}
